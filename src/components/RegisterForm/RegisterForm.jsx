1b8e637a4bd2a39dad2a5d8fa471726e55a37711
Yanira Sanchez <yani@Yaniras-MacBook-Pro.local>
2022-10-13 23:31:29 -0500

    added MUI Components to RegisterForm

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+// MUI Component Imports:
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import Button from '@mui/material/Button';
+
+import registerPhoto from './registerPhoto.jpg'
+
 function RegisterForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -20,41 +29,63 @@ function RegisterForm() {
   }; // end registerUser
 
   return (
-    <form className="formPanel" onSubmit={registerUser}>
-      <h2>Register User</h2>
-      {errors.registrationMessage && (
-        <h3 className="alert" role="alert">
-          {errors.registrationMessage}
-        </h3>
-      )}
-      <div>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            value={username}
-            required
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={password}
-            required
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input className="btn" type="submit" name="submit" value="Register" />
-      </div>
-    </form>
+    <Box className='loginBox'
+      sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      '& > :not(style)': {
+        m: 10,
+        width: '70vw',
+        height: '70vh',
+      },
+    }}
+    >
+      <Paper elevation={3} >
+        <div >
+          <Box
+            className='loginPhoto'
+            sx={{
+              '& > :not(style)': { 
+                width: '45vw',
+                height: '70vh',
+              },
+          }}
+          >
+            <img src={registerPhoto} alt="boy with map"></img>
+          </Box>
+          <Box
+          className='loginForm'
+          sx={{
+            '& > :not(style)': {
+              
+              width: '25vw',
+              height: '70vh',
+            },
+            
+          }}
+          noValidate
+          autoComplete="off"
+          >
+            <form className="loginForm" onSubmit={registerUser}>
+                <InputLabel htmlFor="component-simple">Username</InputLabel>
+                <Input 
+                  id="component-simple" 
+                  value={username} 
+                  onChange={e => setUsername(e.target)} 
+                  />
+                <InputLabel htmlFor="component-simple">Password</InputLabel>
+                <Input 
+                  id="component-simple" 
+                  value={password} 
+                  onChange={e => setPassword(e.target)} 
+                />
+                <Button variant="contained">Login</Button>
+            </form>
+          </Box>
+        </div>
+      </Paper> 
+    </Box>
   );
 }
 
