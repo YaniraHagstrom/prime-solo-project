e048bb716c8e7929e1db09427559541e2eb815ac
Yanira Sanchez <yani@Yaniras-MacBook-Pro.local>
2022-10-13 23:18:18 -0500

    added MUI components to LoginForm component

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
+import './loginPage.css';
+
 
 function LoginPage() {
   const history = useHistory();
@@ -10,7 +12,7 @@ function LoginPage() {
       <LoginForm />
 
       <center>
-        <button
+        {/* <button
           type="button"
           className="btn btn_asLink"
           onClick={() => {
@@ -18,7 +20,7 @@ function LoginPage() {
           }}
         >
           Register
-        </button>
+        </button> */}
       </center>
     </div>
   );
