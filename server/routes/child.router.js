2caf382cc7377818af1d4177f6f7e71f981a612b
Yanira Sanchez <yani@Yaniras-MacBook-Pro.local>
2022-10-17 11:14:34 -0500

    completed and tested POST route for adding child and all of the data from the childForm to the associated data tables

--- server/routes/child.router.js
+++ server/routes/child.router.js
@@ -25,33 +25,40 @@ router.post('/', (req, res)=> {
     // }
 
     const {name, age, primaryLanguage_id, secondaryLanguage_id, services} = childData;
+    console.log('primary:',primaryLanguage_id, 'secondary:', secondaryLanguage_id);
 
     const serviceId = Object.keys(services);
     const checkedStatus = Object.values(services);
-    console.log('testing indexes',serviceId[0],checkedStatus[0]);
+    // console.log('testing indexes',serviceId[0],checkedStatus[0]);
 
-    const sqlQuery = `
-        INSERT INTO children_services ("child_id", "service_id", "checked")
+    const sqlQuery =`
+        WITH ins1 AS (
+            INSERT INTO children ("name", age, user_id)
+                VALUES ($1, $2, $3)
+                RETURNING id AS child_id
+            )
+        , ins2 AS(
+            INSERT INTO children_languages (child_id, language_id)
             VALUES
-                (1, ${serviceId[0]}, ${checkedStatus[0]})
-    ;`
+            ((SELECT child_id FROM ins1), $4),
+            ((SELECT child_id FROM ins1), $5)
+            )
+        INSERT INTO children_services (child_id, service_id, checked)
+        VALUES
+            ((SELECT child_id FROM ins1), ${serviceId[0]}, ${checkedStatus[0]}),
+            ((SELECT child_id FROM ins1), ${serviceId[1]}, ${checkedStatus[1]}),
+            ((SELECT child_id FROM ins1), ${serviceId[2]}, ${checkedStatus[2]}),
+            ((SELECT child_id FROM ins1), ${serviceId[3]}, ${checkedStatus[3]}),
+            ((SELECT child_id FROM ins1), ${serviceId[4]}, ${checkedStatus[4]}),
+            ((SELECT child_id FROM ins1), ${serviceId[5]}, ${checkedStatus[5]}),
+            ((SELECT child_id FROM ins1), ${serviceId[6]}, ${checkedStatus[6]}),
+            ((SELECT child_id FROM ins1), ${serviceId[7]}, ${checkedStatus[7]}),
+            ((SELECT child_id FROM ins1), ${serviceId[8]}, ${checkedStatus[8]}),
+            ((SELECT child_id FROM ins1), ${serviceId[9]}, ${checkedStatus[9]});
+    `
+    const sqlValues = [name, age, userID, primaryLanguage_id, secondaryLanguage_id]
 
-
-    // const sqlQuery =`
-    //     WITH ins1 AS (
-    //         INSERT INTO children ("name", age)
-    //             VALUES ($1, $2)
-    //             RETURNING id AS child_id
-    //         )
-    //     , ins2 AS(
-    //         INSERT INTO children_languages (child_id, language_id)
-    //         VALUES
-    //         ((SELECT child_id FROM ins1), $3),
-    //         ((SELECT child_id FROM ins1), $4);
-    // `
-    // const sqlValues = [name, age, primaryLanguage_id, secondaryLanguage_id]
-
-    pool.query(sqlQuery)
+    pool.query(sqlQuery, sqlValues)
         .then(dbRes => {
             res.sendStatus(201);
         })
